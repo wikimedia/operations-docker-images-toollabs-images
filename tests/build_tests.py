078c5f4f487ecfb5cd5975d2a98ec137b2ffccbd
Bryan Davis <bd808@wikimedia.org>
2019-08-27 17:52:42 -0600

    Add black to tox
    Add a tox environment that will run black in diff mode to show
    formatting changes that should be applied to the codebase.
    A pyproject.toml configuration file is also added to codify the tweaks
    to black's defaults which the WMCS team is currently using.
    
    The existing tests/build_tests.py script is also changed here by
    applying the diff suggested by black.
    
    Change-Id: I08da49ba9f53c4e7211505d84455fc347f6d7a2a

--- tests/build_tests.py
+++ tests/build_tests.py
@@ -9,44 +9,46 @@ def assert_same(result, expect):
 
 def test_lineage_of():
     build.IMAGES = {
-        'base': {
-            'A': ['A1', 'A2'],
-            'B': ['B1', 'B2'],
-            'C': {
-                'C1': {'C1a': ['C1a1']},
-                'C2': ['C2a'],
-            }
+        "base": {
+            "A": ["A1", "A2"],
+            "B": ["B1", "B2"],
+            "C": {"C1": {"C1a": ["C1a1"]}, "C2": ["C2a"]},
         }
     }
 
-    assert_equals(build.lineage_of('not_found'), None)
+    assert_equals(build.lineage_of("not_found"), None)
     assert_same(
-        build.lineage_of('base'),
+        build.lineage_of("base"),
         [
-            'base',
-            'A', 'A1', 'A2',
-            'B', 'B1', 'B2',
-            'C', 'C1', 'C1a', 'C1a1', 'C2', 'C2a',
-        ]
+            "base",
+            "A",
+            "A1",
+            "A2",
+            "B",
+            "B1",
+            "B2",
+            "C",
+            "C1",
+            "C1a",
+            "C1a1",
+            "C2",
+            "C2a",
+        ],
     )
-    assert_same(
-        build.lineage_of('B1'),
-        ['base', 'B', 'B1']
-    )
-    assert_same(
-        build.lineage_of('C1'),
-        ['C1a1', 'C1a', 'C1', 'C', 'base']
-    )
-    assert_same(
-        build.lineage_of('C1a1'),
-        ['C1a1', 'C1a', 'C1', 'C', 'base']
-    )
+    assert_same(build.lineage_of("B1"), ["base", "B", "B1"])
+    assert_same(build.lineage_of("C1"), ["C1a1", "C1a", "C1", "C", "base"])
+    assert_same(build.lineage_of("C1a1"), ["C1a1", "C1a", "C1", "C", "base"])
 
 
 def test_make_docker_tag():
     tests = {
-        'registry/prefix-name:latest': ['name', 'registry', 'prefix', 'latest'],
-        'test/prefix-foo-bar-baz:stable': ['foo/bar/baz', 'test', 'prefix', 'stable'],
+        "registry/prefix-name:latest": ["name", "registry", "prefix", "latest"],
+        "test/prefix-foo-bar-baz:stable": [
+            "foo/bar/baz",
+            "test",
+            "prefix",
+            "stable",
+        ],
     }
     for expect, args in tests.items():
         yield check_make_docker_tag, args, expect
@@ -59,10 +61,9 @@ def check_make_docker_tag(given, expect):
 def test_expand_template():
     template = "foo:{foo}\nbar:{bar}"
     assert_equals(
-        build.expand_template(template, {'foo': 1, 'bar': 2}),
-        "foo:1\nbar:2"
+        build.expand_template(template, {"foo": 1, "bar": 2}), "foo:1\nbar:2"
     )
     assert_equals(
-        build.expand_template(template, {'foo': 1, 'bar': 2, 'baz': 3}),
-        "foo:1\nbar:2"
+        build.expand_template(template, {"foo": 1, "bar": 2, "baz": 3}),
+        "foo:1\nbar:2",
     )
