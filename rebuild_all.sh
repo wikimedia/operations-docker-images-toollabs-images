02c3cf37ab3f6249de6af189744390db160842ee
Bryan Davis <bd808@wikimedia.org>
2020-01-02 19:49:01 -0700

    support tools: Add script to rebuild all images
    This is getting to be more and more cumbersome, so let's make it
    a slightly more repeatable process.
    
    Change-Id: Ic5181413d4671ea78417f64984938cb26601fa9a

---
+++ rebuild_all.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Rebuild all Docker images in this repo
+
+set -Eeuo pipefail
+
+clean_docker_layers () {
+    docker ps --no-trunc -aqf "status=exited" | xargs docker rm
+    docker images --no-trunc | awk '$2=="<none>" { print $3 }' |
+        xargs -r docker rmi
+    docker images -f "dangling=true" -q |
+        xargs -r docker rmi
+}
+
+for series in base stretch buster; do
+    # Images for the legacy k8s cluster
+    ./build.py --image-prefix toollabs --tag latest --no-cache --push $series
+    clean_docker_layers
+done
+
+for series in jessie-sssd stretch-sssd buster-sssd; do
+    # Images for the 2020 k8s cluster
+    ./build.py --image-prefix toolforge --tag latest --no-cache --push $series
+    clean_docker_layers
+done
