7b8688872067eeaee969a0a488e4a8e6926bda0f
Bryan Davis <bd808@wikimedia.org>
2020-02-13 09:45:26 -0700

    rebuild_all: Ignore errors in Docker layer pruning
    Bug: T244795
    Change-Id: Iaeb0db69bb3719331f7014764743611876c0fbd8

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -14,12 +14,16 @@ clean_docker_layers () {
 
 for series in base stretch; do
     # Images for the legacy k8s cluster
+    echo "=== START toollabs ${series} ==="
     ./build.py --image-prefix toollabs --tag latest --no-cache --push $series
-    clean_docker_layers
+    clean_docker_layers || /bin/true
+    echo "=== END toollabs ${series} ==="
 done
 
 for series in jessie-sssd stretch-sssd buster buster-sssd; do
-    # Images for the 2020 k8s cluster
+    # Images for the 2020 k8s cluster (+buster for the legacy cluster)
+    echo "=== START toolforge ${series} ==="
     ./build.py --image-prefix toolforge --tag latest --no-cache --push $series
-    clean_docker_layers
+    clean_docker_layers || /bin/true
+    echo "=== END toolforge ${series} ==="
 done
